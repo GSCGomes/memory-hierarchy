2d57c58be1b53c013153a4faf1e8a08bb27d3f56
Guilherme Gomes <guilhermescgomes@gmail.com>
2022-02-12 21:20:44 -0300

    deleted makefile from release branch and added dados.txt and run.sh

--- Makefile
+++
@@ -1,9 +0,0 @@
-CXX := g++
-CFLAGS := -Wall -std=c++17
-EXE_NAME := mh
-
-all:
-	$(CXX) $(CFLAGS) -o $(EXE_NAME) main.cpp
-
-clean:
-	$(RM) $(EXE_NAME)
---
+++ run.sh
@@ -0,0 +1,11 @@
+# TP3 - OC1
+# Guilherme Serra Camargo Gomes
+# Felipe Aragão Nogueira de Freitas
+
+# Script para compilação e execução, é necessário ter versão do GCC igual ou superior a gcc 5.0
+
+# para compilar basta utilizar esse comando:
+g++ -Wall -std=c++17 main.cpp -o mh.exe
+
+# para executar basta utilizar o executável gerado e o nome do arquivo de entrada
+./mh.exe arquivo.txt
